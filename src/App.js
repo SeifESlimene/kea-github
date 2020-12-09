68216039a441514c6dd139fa198605c72b60e24c
SeifESlimene <s.slimene19@gmail.com>
2020-12-09 18:17:57 +0100

    new kea repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { kea, useActions, useValues } from 'kea'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const logic = kea({
+    actions: {
+        setUsername: (username) => ({ username })
+    },
+
+    reducers: {
+        username: ['keajs', {
+            setUsername: (_, payload) => payload.username
+        }]
+    }
+})
+
+export default function Github () {
+    const { username } = useValues(logic)
+    const { setUsername } = useActions(logic)
+
+    return (
+        <div className='example-github-scene'>
+            <div style={{marginBottom: 20}}>
+                <h1>Search for a github user</h1>
+                <input
+                    value={username}
+                    type='text'
+                    onChange={e => setUsername(e.target.value)} />
+            </div>
+            <div>
+                Repos will come here for user <strong>{username}</strong>
+            </div>
+        </div>
+    )
 }
-
-export default App;
